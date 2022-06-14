9489d9682e80670721781fc683979d5195d0c31d
chang8128 <chang8128@sina.cn>
2022-06-14 16:16:17 +0800

    SwiftUI Essentials, lesson 1, Creating and Combining Views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 常志平 on 14/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
