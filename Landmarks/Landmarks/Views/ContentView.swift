f16902ea672715bd18dbaaf369fe7bdbd79755a2
chang8128 <chang8128@sina.cn>
2022-06-15 23:42:16 +0800

    course two completed. draw the UML diagram.

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 常志平 on 14/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewInterfaceOrientation(.portrait)
+    }
+}
